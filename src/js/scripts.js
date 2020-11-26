a0c26553e29dd367b912861609a82f25e9892c2c
unknown <geka.shatalov@gmail.com>
2020-11-26 16:13:28 +0100

    js bntTrigger

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,36 @@
 window.onload = function(){
     let btnNav = $('.nav-trigger');
-    let btnNavOpen = $('.menu');
-    btnNav.on('click', open);
+    let btnNavOpen = new Trigger($('.header_menu'));
+
+    btnNav.on('click', btnNavOpen.active);
 
 
-    function open (){
-        btnNavOpen.toggleClass('open');
-    }
+    let btnBuy = $('.best__deals_body-card-buy');
+    console.log(btnBuy);
 
+    btnBuy.on('click', function(){
+        console.log($(this).parents().eq(1).find('.best__deals_body-card-price-actual').html());
+    });
+
+
+};
+
+function Trigger(block, span){
+    this.block = block;
+    this.span = span;
+    let context = this;
+
+    context.active = function(){
+        if(context.span){
+            context.span.toggleClass('open');
+            context.block.slideToggle();
+        } else {
+            context.block.slideToggle();
+            
+        }
+    };
+
+    // context.onlySlide = function(){
+    //     context.block.slideToggle();
+    // };
 }
